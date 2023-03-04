cfc9e2523cad64b71f68845e0b9dca15231c8d6e
Danish <tabcdef142@gmail.com>
2023-03-04 23:39:47 +0530

    first commit in Arithmetic operators

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 console.log('hello javascript');
 console.log(2+2);
+
+//creating objects in javascript
+let user={
+ name:"Tom",
+ age:"30"
+};
+
+console.log(user);
+
+//Access the name & age using dot notation
+console.log(user.name);
+console.log(user.age);
+//Dynamic typing
+user.name="Naveen"
+console.log(user.name);
+console.log(user.name + ' '+user.age);
+
+//Arrays(List of objects or collection of similar types of data)
+
+let Arr=['java','javascript'];
+console.log(Arr);
+console.log(Arr.length);
+Arr[2]='python';
+console.log(Arr);
