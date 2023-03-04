0f06a464eab0e735de640be140f38a5c93cfa624
Danish <tabcdef142@gmail.com>
2023-03-05 00:16:45 +0530

    first commit in Arithmetic operators

---
+++ operators.js
@@ -0,0 +1,20 @@
+//Arithmetic Operators
+
+let x=10;
+let y=5;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x**y);   //10 to the power 5
+
+// Increment and decrement(++ & --)
+
+let a=1;
+console.log(a++);
+console.log(a);
+
+let p=1;
+let q=p++;
+console.log(p);
+console.log(q);
